9c24c772a915422bf93fc44f3b0ad8efe9378342
jrm-zero <github@jrsort.com>
2026-06-08 13:43:31 -0500

    in-progress translation, boot.dev streak commit

---
+++ src/translation_functions.py
@@ -0,0 +1,41 @@
+from doc_handling import *
+from google.cloud import translate_v3
+import os
+
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+def translate(
+        text, 
+        source_language_code: str = "en-US", 
+        target_language_code: str = "fr"
+        ) -> translate_v3.TranslationServiceClient:
+    """
+    Translate Text from a Source language to a Target language.
+    Args:
+        text: The content to translate.
+        source_language_code: The code of the source language.
+        target_language_code: The code of the target language.
+            For example: "fr" for French, "es" for Spanish, etc.
+            Find available languages and codes here:
+            https://cloud.google.com/translate/docs/languages#neural_machine_translation_model
+    """
+    
+    #Initialize Translation Client
+    client = translate_v3.TranslationServiceClient()
+    parent = f"projects/{PROJECT_ID}/locations/global"
+
+    #MIME type of the content to translate.
+    #Supported MIME types:
+    #https://cloud.google.com/translate/docs/supported-formats
+    mime_type = "text/plain"
+
+    #Translate text from the source to the target language.
+    response = client.translate_text(
+        contents=[text],
+        parent=parent,
+        mime_type=mime_type,
+        source_language_code=source_language_code,
+        target_language_code=target_language_code,
+    )
+
+    return response
